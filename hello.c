23e44058549e8782f83d8d5370bd341b85538eec
priyatank298 <33170811+priyatank298@users.noreply.github.com>
2017-10-28 10:49:05 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+//clear privious screen output
+clrscr();
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+
+//hold screen for output
+getch();
+}
